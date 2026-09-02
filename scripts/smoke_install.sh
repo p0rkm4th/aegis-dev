c157b785043b73a4ea7f16ea9c58bdaf4a96965f
p0rkm4th <scott.greer@zoho.com>
2026-09-02 13:48:37 -0500

    Advance clean install migration gate

--- scripts/smoke_install.sh
+++ scripts/smoke_install.sh
@@ -21,7 +21,7 @@ sdist="$(find "$dist_dir" -maxdepth 1 -type f -name '*.tar.gz' -print -quit)"
 test -n "$wheel"
 test -n "$sdist"
 python -m pip install --no-deps --target "$install_dir" "$wheel"
-PYTHONPATH="$install_dir" python -c 'from aegis.migrations import validate_migrations; migrations = validate_migrations(); assert migrations and migrations[-1] == "014_security_lab_findings.sql"'
+PYTHONPATH="$install_dir" python -c 'from aegis.migrations import validate_migrations; migrations = validate_migrations(); assert migrations and migrations[-1] == "016_audit_chain_head.sql"'
 
 python -m pip install --no-deps --prefix "$prefix_dir" "$wheel"
 site_dir="$(find "$prefix_dir" -type d -path '*/site-packages' -print -quit)"
@@ -34,4 +34,4 @@ python -m pip install --no-deps --prefix "$sdist_prefix_dir" "$sdist"
 sdist_site_dir="$(find "$sdist_prefix_dir" -type d -path '*/site-packages' -print -quit)"
 test -x "$sdist_prefix_dir/bin/aegis"
 PYTHONPATH="$sdist_site_dir" "$sdist_prefix_dir/bin/aegis" --help >/dev/null
-PYTHONPATH="$sdist_site_dir" python -c 'from aegis.migrations import validate_migrations; assert validate_migrations()[-1] == "014_security_lab_findings.sql"'
+PYTHONPATH="$sdist_site_dir" python -c 'from aegis.migrations import validate_migrations; assert validate_migrations()[-1] == "016_audit_chain_head.sql"'
